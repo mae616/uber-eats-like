1141dfe000fee80ddd39774d1f9e74d41ce94278
mae616 <oya2.mi@hotmail.co.jp>
2021-12-25 12:08:25 +0900

    ルーティングした時にparamsを渡す方法

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,6 +18,7 @@ function App() {
         <Route path='restaurants' element={<Restaurants />} />
         <Route path='foods' element={<Foods />} />
         <Route path='orders' element={<Orders />} />
+        <Route path='restaurants/:restaurantsId/foods' element={<Foods />} />
       </Routes>
     </Router>
   );
--- frontend/src/containers/Foods.jsx
+++ frontend/src/containers/Foods.jsx
@@ -1,9 +1,15 @@
 import { Fragment } from 'react';
+import { useParams } from 'react-router-dom';
 
 export const Foods = () => {
+  const { restaurantsId } = useParams();
+
   return (
     <Fragment>
       フード一覧
+      <p>
+        restaurantIdは {restaurantsId} です
+      </p>
     </Fragment>
   );
 };
