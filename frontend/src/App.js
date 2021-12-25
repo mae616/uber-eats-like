447f685ca3e9ead9098a249ca63263f1af0b8005
mae616 <oya2.mi@hotmail.co.jp>
2021-12-25 11:54:00 +0900

    ベーシックなルーティング

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom';
+
+// components
+import { Restaurants } from './containers/Restaurants';
+import { Foods } from './containers/Foods';
+import { Orders } from './containers/Orders';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          aaaEdit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='restaurants' element={<Restaurants />} />
+        <Route path='foods' element={<Foods />} />
+        <Route path='orders' element={<Orders />} />
+      </Routes>
+    </Router>
   );
 }
 
