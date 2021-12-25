9d87e2455591b9a41551b2f112f260c62b1a001d
mae616 <oya2.mi@hotmail.co.jp>
2021-12-25 12:41:46 +0900

    定数の定義、画像のフォルダ

---
+++ frontend/src/constants.js
@@ -0,0 +1,12 @@
+// APIリクエストに関して必要になる定数
+
+// APIリクエスト中に画面がいまどういう状態なのか？を知るための状態
+export const REQUEST_STATE = {
+  INITIAL: 'INITIAL',
+  LOADING: 'LOADING',
+  OK: 'OK'
+}
+
+export const HTTP_STATUS_CODE = {
+  NOT_ACCEPTABLE: 406
+}
