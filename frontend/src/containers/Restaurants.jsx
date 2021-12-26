20153de9fc99ee1546da07f2c8c84f62dbf199c1
mae616 <oya2.mi@hotmail.co.jp>
2021-12-26 20:20:09 +0900

    レストラン一覧画面: styled-componentsの導入、useReducer

--- frontend/src/containers/Restaurants.jsx
+++ frontend/src/containers/Restaurants.jsx
@@ -1,19 +1,71 @@
-import { Fragment, useEffect } from 'react';
+import { Fragment, useEffect, useReducer } from 'react';
+import styled from 'styled-components';
 
 // apis
 import { fetchRestaurants } from '../apis/restaurants';
 
+// reducers
+import {
+  initialState,
+  restaurantsActionTypes,
+  restaurantsReducer
+} from '../reducers/restaurants';
+
+// images
+import MainLogo from '../images/logo.png';
+import MainCoverImage from '../images/main-cover-image.png';
+
+const HeaderWrapper = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  padding: 8px 32px;
+`;
+
+const MainLogoImage = styled.img`
+  height: 90px;
+`;
+
+const MainCoverImageWrapper = styled.div`
+  text-align: center;
+`;
+
+const MainCover = styled.img`
+  height: 600px;
+`;
+
 export const Restaurants = () => {
+  const [state, dispatch] = useReducer(restaurantsReducer, initialState)
 
   useEffect(() => {
+    dispatch({ type: restaurantsActionTypes.FETCHING });
+
     fetchRestaurants()
       .then((data) =>
-        console.log(data))
+        // Reducerのディスパッチ
+        dispatch({
+          type: restaurantsActionTypes.FETCH_SUCCESS,
+          payload: {
+            restaurants: data.restaurants
+          }
+        })
+      )
   }, []);
 
   return (
     <Fragment>
-      レストラン一覧
+      <HeaderWrapper>
+        <MainLogoImage src={MainLogo} alt="main log" />
+      </HeaderWrapper>
+      <MainCoverImageWrapper>
+        <MainCover src={MainCoverImage} alt="main cover" />
+      </MainCoverImageWrapper>
+      {
+        state.restaurantsList.map(restaurant => (
+          <div key={restaurant.id}>
+            {restaurant.name}
+          </div>
+        ))
+      }
     </Fragment>
   );
 };
