bee6e42180281b6609d4e86040bee8c8a0c6045a
mae616 <oya2.mi@hotmail.co.jp>
2021-12-27 19:11:26 +0900

    フード一覧画面: APIをコンソール出力

---
+++ frontend/src/apis/foods.js
@@ -0,0 +1,11 @@
+// フード一覧のAPIを呼ぶ
+import axios from 'axios';
+import { foodsIndex } from '../urls/index';
+
+export const fetchFoods = (restaurantId) => {
+  return axios.get(foodsIndex(restaurantId))
+    .then(res => {
+      return res.data
+    })
+    .catch((e) => console.error(e))
+}
--- frontend/src/containers/Foods.jsx
+++ frontend/src/containers/Foods.jsx
@@ -1,8 +1,18 @@
-import { Fragment } from 'react';
+import { Fragment, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+
+// apis
+import { fetchFoods } from '../apis/foods';
 
 export const Foods = () => {
   const { restaurantsId } = useParams();
+
+  useEffect(() => {
+    fetchFoods(1)
+      .then((data) =>
+        console.log(data)
+      )
+  }, []);
 
   return (
     <Fragment>
