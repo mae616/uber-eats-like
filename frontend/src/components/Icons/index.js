a68f6243600154614befa4015b05112f8981c120
mae616 <oya2.mi@hotmail.co.jp>
2021-12-27 20:48:58 +0900

    フード一覧画面: レイアウト、コンポーネントの分割

---
+++ frontend/src/components/Icons/index.js
@@ -0,0 +1,3 @@
+// Material UI から取り込んだアイコン一覧を定義
+export { default as LocalMallIcon } from '@material-ui/icons/LocalMall';
+export { default as QueryBuilderIcon } from '@material-ui/icons/QueryBuilder';
