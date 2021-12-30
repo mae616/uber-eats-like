3972de6df162c813fe17f56471fae1e6ee1ce791
mae616 <oya2.mi@hotmail.co.jp>
2021-12-30 18:40:20 +0900

    フード一覧画面: ボタンコンポーネントの作成

---
+++ frontend/src/components/Buttons/CountDownButton.jsx
@@ -0,0 +1,11 @@
+// style
+import { RoundButton } from '../shared_style';
+
+export const CountDownButton = ({
+  onClick,
+  isDisabled
+}) => (
+  <RoundButton onClick={onClick} disabled={isDisabled}>
+    ー
+  </RoundButton>
+)
---
+++ frontend/src/components/Buttons/CountUpButton.jsx
@@ -0,0 +1,11 @@
+// style
+import { RoundButton } from '../shared_style';
+
+export const CountUpButton = ({
+  onClick,
+  isDisabled
+}) => (
+  <RoundButton onClick={onClick} disabled={isDisabled}>
+    ＋
+  </RoundButton>
+);
